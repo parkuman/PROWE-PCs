ba0bea519eaae1295ea2d65bce673ab05a549782
Parkuman <parkumania@gmail.com>
2020-06-02 12:49:03 -0400

    Updates

---
+++ precache-manifest.4f6714f3bfb7823c2f5fbcc9aeef9cfd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47de8c6babed82620036fad2038c1c6c",
+    "url": "/PROWE-PCs/index.html"
+  },
+  {
+    "revision": "e167c6ae10f360a92e0b",
+    "url": "/PROWE-PCs/static/css/main.2ec6df8d.chunk.css"
+  },
+  {
+    "revision": "d8f2789083b2f07c808d",
+    "url": "/PROWE-PCs/static/js/2.e7782714.chunk.js"
+  },
+  {
+    "revision": "73fcec6f3c94448eba04adf712b18ac2",
+    "url": "/PROWE-PCs/static/js/2.e7782714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e167c6ae10f360a92e0b",
+    "url": "/PROWE-PCs/static/js/main.073e51a9.chunk.js"
+  },
+  {
+    "revision": "4d6f037652689a9ec8a3",
+    "url": "/PROWE-PCs/static/js/runtime-main.2cb61e4a.js"
+  },
+  {
+    "revision": "1d9b620b56c9d0b89960bd25f0c0a8b0",
+    "url": "/PROWE-PCs/static/media/bg-shadow.1d9b620b.svg"
+  },
+  {
+    "revision": "6221e897eab4f8a9914c67ce06ccdd9e",
+    "url": "/PROWE-PCs/static/media/logo.6221e897.svg"
+  },
+  {
+    "revision": "865839e7360fbf748694e2a7ce0fb880",
+    "url": "/PROWE-PCs/static/media/profilepic.865839e7.jpg"
+  }
+]);
